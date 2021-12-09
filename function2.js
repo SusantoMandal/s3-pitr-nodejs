32bfe70eced32458ccfa86640c46cf9cdb4e39a9
Susanto Mandal <susanto.mandal@comprotechnologies.com>
2021-12-09 16:45:32 +0530

    merge two functions

--- function2.js
+++ function2.js
@@ -1,30 +1,21 @@
-var AWS = require('aws-sdk');
-s3 = new AWS.S3({
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
 });
 
-const cobj = {
-    ETag: '"168a64fad8930480043d4366697698ca"',
-    Size: 21,
-    StorageClass: 'STANDARD',
-    Key: 'restore/a.txt',
-    VersionId: 'WDizAvqpY_PtUbI_gsiZmrtwENzL5Riy',
-    IsLatest: false, 
-    LastModified: '2021-12-09T07:45:11.000Z',
-    Owner: [Object]
-  }
-
-const arguments = {
-    sourceBucket: process.argv[2],
-    fileName: cobj.Key,
-    versionId: cobj.VersionId
+async function restoreObject(version, bucket) {
+    try {
+        const params = {
+            Bucket: bucket,
+            CopySource: `${bucket}/${version.Key}?versionId=${version.VersionId}`,
+            Key: version.Key,
+        };
+    const data = await s3.copyObject(params).promise();
+    console.log("Folder Updated");
+    } catch (e) {
+        throw new Error(`Could not restore file from S3: ${e.message}`)
+    }
 }
-var params = {
-    Bucket: arguments.sourceBucket, 
-    CopySource: `${arguments.sourceBucket}/${cobj.Key}?versionId=${cobj.VersionId}`,
-    Key: cobj.Key,
-};
 
-s3.copyObject(params, function(err,data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-})
+module.exports = { restoreObject };
+
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const { getVersion } = require('./function1');
+const { restoreObject } = require('./function2');
 
 const sourceBucket = process.argv[2];
 const folder = process.argv[3];
@@ -7,5 +8,5 @@ const timestamp = process.argv[4];
 
 (async () => {
   const version = await getVersion(sourceBucket, folder, timestamp);
-  console.log(version);
+  await restoreObject(version, sourceBucket);
 })();
